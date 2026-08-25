edaee3587543afc7bb060866e4b655ee9b7b6eef
Wareon-top <dgalil401@gmail.com>
2026-08-25 21:10:24 +0300

    style: load monetization center styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App';
 import './styles.css';
 import './channels.css';
 import './publishing.css';
+import './monetization.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
